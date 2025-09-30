1bacd5a896ae87f802768334c77109a2244cbf71
khainq39 <qkn3018@gmail.com>
2025-09-30 11:31:48 +0700

    Update viettelService.js

--- services/viettelService.js
+++ services/viettelService.js
@@ -1,1 +1,30 @@
 
+import axios from "axios";
+
+const BASE_URL = "https://partner.viettelpost.vn/v2";
+
+// Hàm call chung
+async function apiCall(endpoint, body, token) {
+  const headers = {
+    "Content-Type": "application/json",
+    ...(token ? { Authorization: `Bearer ${token}` } : {})
+  };
+
+  const { data } = await axios.post(`${BASE_URL}${endpoint}`, body, { headers });
+  return data;
+}
+
+export const viettelService = {
+  login: (username, password) =>
+    apiCall("/user/Login", { USERNAME: username, PASSWORD: password }),
+
+  getPrice: (payload, token) => apiCall("/order/getPrice", payload, token),
+
+  createOrder: (payload, token) => apiCall("/order/createOrder", payload, token),
+
+  updateOrder: (payload, token) => apiCall("/order/UpdateOrder", payload, token),
+
+  trackOrder: (payload, token) => apiCall("/order/getOrderInfo", payload, token),
+
+  printOrder: (payload, token) => apiCall("/order/print", payload, token)
+};
